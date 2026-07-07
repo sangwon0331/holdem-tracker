d242929b65527aa6bcd95e175f673d8191e1ef81
sangwon <sangwon0332@gmail.com>
2026-07-07 15:49:03 +0900

    스테이킹 지분, 경비, 대회 프리셋, CSV 내보내기, 컨디션 기록, 달력 뷰, 연도별/컨디션별 통계, 멀티 통화 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'holdem-tracker-v3';
+const CACHE = 'holdem-tracker-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
