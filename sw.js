5bb63645b46b37319096a200c2c1300726209fcf
sangwon <sangwon0332@gmail.com>
2026-07-07 14:17:28 +0900

    차트 기간 필터, 장소별 수익 통계, 리엔트리 횟수 기록, 멀티데이 결과 대기 상태 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'holdem-tracker-v2';
+const CACHE = 'holdem-tracker-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
